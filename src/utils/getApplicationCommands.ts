5c99f5489ac12f8409fd72b498f3dfdd6d8fadb9
AmadeusMoon <sunnysneezer@gmail.com>
2023-06-18 00:34:16 +0200

    Installed types for discord

--- src/utils/getApplicationCommands.ts
+++ src/utils/getApplicationCommands.ts
@@ -1,11 +1,10 @@
-module.exports = async (client, guildId) => {
+module.exports = async (client: string, guildId: string) => {
   let applicationCommands;
 
   if (guildId) {
-    const guild = await client.guilds.fetch(guildId);
+    const guild = (await client.guilds.fetch(guildId)) as string;
     applicationCommands = guild.commands;
-  }
-  else {
+  } else {
     applicationCommands = await client.application.commands;
   }
 
