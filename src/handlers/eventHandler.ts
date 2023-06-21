1c211b7e11aa91a1c1d7b917168be5095d12bed2
AmadeusMoon <sunnysneezer@gmail.com>
2023-06-21 21:43:14 +0200

    Yea

--- src/handlers/eventHandler.ts
+++ src/handlers/eventHandler.ts
@@ -1,18 +1,29 @@
 import path from 'path';
+import { Client } from 'discord.js';
 import getAllFiles from '../utils/getAllFiles';
 
-const eventHandler = (client: any) => {
-  const eventFolders = getAllFiles(path.join(__dirname, '..', 'events'), true);
-
-  for (const eventFolder of eventFolders) {
-    const eventFiles = getAllFiles(eventFolder);
-    eventFiles.sort();
+const eventHandler = function (client: Client) {
+  const eventFolders = getAllFiles(
+    path.join(__dirname, '../events'),
+    true
+  ) as string[];
+  //Goes 2 folders up enters events and lists folders inside
+  for (const eventFolder of eventFolders!) {
+    //Goes thru each event folder separately
+    const eventFiles = getAllFiles(eventFolder) as string[];
+    eventFiles.sort((a, b) => (a > b ? 1 : -1));
+    //Uses getAllFiles which returns path of all files and folders inside
+    //the folder to define eventFiles then sorts
 
     const eventName = eventFolder.replace(/\\/g, '/').split('/').pop()!;
+    //Replaces path to folder with name of last destination
+    //(removes the / and everything before them )
 
     client.on(eventName, async (arg: any) => {
       for (const eventFile of eventFiles) {
+        //For each TS file from the folder
         const eventFunction = require(eventFile);
+        //Create a function that uses that requires that file as a module
         await eventFunction(client, arg);
       }
     });
--- src/utils/getAllFiles.ts
+++ src/utils/getAllFiles.ts
@@ -1,26 +1,23 @@
 import fs from 'fs';
 import path from 'path';
 
-const getAllFiles = (directory: string, foldersOnly = false) => {
+const getAllFile = (directory: any, foldersOnly = false) => {
   const fileNames = [];
 
   const files = fs.readdirSync(directory, { withFileTypes: true });
+  //Reads directory and populates it with names of files and directories inside it
 
   for (const file of files) {
     const filePath = path.join(directory, file.name);
+    //Creates path for files
 
-    if (foldersOnly) {
-      if (file.isDirectory()) {
-        fileNames.push(filePath);
-      }
-    } else {
-      if (file.isFile()) {
-        fileNames.push(filePath);
-      }
+    if ((foldersOnly && file.isDirectory()) || (foldersOnly && file.isFile())) {
+      fileNames.push(filePath);
+      //Populates filenames array with paths to files
     }
   }
-
   return fileNames;
 };
 
+export const getAllFiles = getAllFile;
 export default getAllFiles;
