a07b14969e03bda1d699f54bb4c4ac28a39677f8
AmadeusMoon <sunnysneezer@gmail.com>
2023-06-23 05:16:25 +0200

    Final form is complete magic

--- src/utils/getApplicationCommands.ts
+++ src/utils/getApplicationCommands.ts
@@ -1,23 +1,17 @@
-import {
-  ApplicationCommandManager,
-  Client,
-  ClientApplication,
-  GuildApplicationCommandManager,
-} from 'discord.js';
+import { Client } from 'discord.js';
 
 module.exports = async (client: Client, guildId: string) => {
   let applicationCommands;
 
   if (guildId) {
     const guild = await client.guilds.fetch(guildId);
-    applicationCommands = guild.commands as GuildApplicationCommandManager;
+    applicationCommands = guild.commands;
   } else {
-    applicationCommands = (await (client.application as ClientApplication)
-      .commands) as ApplicationCommandManager;
+    applicationCommands = await client.application!.commands;
   }
   //Checking to see if the bot is inside a server or private DM before showing
   //respective commands for chat
 
-  await fetch(applicationCommands as any);
+  await applicationCommands.fetch(applicationCommands as any);
   return applicationCommands;
 };
