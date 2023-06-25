ad9fcdb65ca82ebac0831f6223aacd7d73d16bbc
Amadeus <119727059+AmadeusMoon@users.noreply.github.com>
2023-06-26 01:30:25 +0200

    Update source.ts to work
    Imported Client for type and added _client to callback

--- src/commands/info/source.ts
+++ src/commands/info/source.ts
@@ -1,11 +1,11 @@
 require('dotenv').config();
 require('discord.js');
-import { CommandInteraction } from 'discord.js';
+import { Client, CommandInteraction } from 'discord.js';
 
 module.exports = {
   /* @param {Client} client
    * @param {Interaction} interaction */
-  callback: (interaction: CommandInteraction) => {
+  callback: (_client: Client, interaction: CommandInteraction) => {
     console.log('Get Source');
     interaction.reply(process.env.SOURCE);
   },
